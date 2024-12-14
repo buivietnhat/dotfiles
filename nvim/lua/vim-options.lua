e8d824758b05caf46bbf4bea06a076c0f072d4ca
buivietnhat <buivietnhatctqb@gmail.com>
2024-12-14 18:47:44 +0700

    Show relative line number

--- nvim/lua/vim-options.lua
+++ nvim/lua/vim-options.lua
@@ -4,3 +4,19 @@ vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
 vim.g.mapleader = " "
 vim.o.showmode = false
+
+vim.wo.number = true        -- Absolute line numbers
+vim.wo.relativenumber = true  -- Relative line numbers
+
+-- case-insensitive search/replace
+vim.opt.ignorecase = true
+
+-- unless uppercase in search term
+vim.opt.smartcase = true
+
+-- always draw sign column. prevents buffer moving when adding/deleting sign
+vim.opt.signcolumn = 'yes'
+
+
+
+

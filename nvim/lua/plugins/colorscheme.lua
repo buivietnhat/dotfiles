ff8e33b912d9ad9972b11c78db9603ee77a127da
buivietnhat <buivietnhatctqb@gmail.com>
2024-12-20 18:12:36 +0700

    add adjust colorscheme

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -7,10 +7,24 @@ return {
         vim.o.background = 'dark'
         -- XXX: hi Normal ctermbg=NONE
         -- Make comments more prominent -- they are important.
-        local bools = vim.api.nvim_get_hl(0, { name = 'Boolean' })
-        vim.api.nvim_set_hl(0, 'Comment', bools)
+        -- local bools = vim.api.nvim_get_hl(0, { name = 'Boolean' })
+        -- print(vim.inspect(bools))
+        vim.api.nvim_set_hl(0, 'Comment', { ctermfg = 6 }) 
+        vim.api.nvim_set_hl(0, 'Repeat', { ctermfg = 5 }) 
+        vim.api.nvim_set_hl(0, 'StorageClass', { ctermfg = 5 }) 
+        vim.api.nvim_set_hl(0, 'Macro', { ctermfg = 3 }) 
+        vim.api.nvim_set_hl(0, 'Identifier', { ctermfg = 3 }) 
+        vim.api.nvim_set_hl(0, 'Character', { ctermfg = 3 }) 
+        vim.api.nvim_set_hl(0, 'SignColumn', { ctermfg = 0, ctermbg = 0 }) 
+        vim.api.nvim_set_hl(0, 'LineNr', { ctermfg = 8, ctermbg = 0 }) 
+        -- vim.api.nvim_set_hl(0, 'SignColumn', { ctermfg = 0 }) 
         -- Make it clearly visible which argument we're at.
         local marked = vim.api.nvim_get_hl(0, { name = 'PMenu' })
         vim.api.nvim_set_hl(0, 'LspSignatureActiveParameter', {})
     end
 }
+
+
+
+
+

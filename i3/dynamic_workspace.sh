1431a2fff2dfa8dd59cf93a8882ce7569b623f93
buivietnhat <buivietnhatctqb@gmail.com>
2025-02-21 19:05:39 +0700

    update new config

--- i3/dynamic_workspace.sh
+++ i3/dynamic_workspace.sh
@@ -12,9 +12,9 @@ PRIMARY=$(xrandr --query | grep " primary" | awk '{print $1}')
 SECONDARY=$(xrandr --query | grep " connected" | grep -v " primary" | awk '{print $1}')
 
 # Assign workspaces to monitors
-i3-msg --socket="$SOCKET" "workspace 1; move workspace to output $PRIMARY"
-
+i3-msg --socket="$SOCKET" "workspace I; move workspace to output $PRIMARY"
 if [[ -n "$SECONDARY" ]]; then
-    i3-msg --socket="$SOCKET" "workspace 2; move workspace to output $SECONDARY"
+    i3-msg --socket="$SOCKET" "workspace IX; move workspace to output $SECONDARY"
 fi
+
 
--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -7,19 +7,21 @@ return {
 		vim.o.background = "dark"
 		-- XXX: hi Normal ctermbg=NONE
 		-- Make comments more prominent -- they are important.
-		-- local bools = vim.api.nvim_get_hl(0, { name = 'Boolean' })
-		-- print(vim.inspect(bools))
 		vim.api.nvim_set_hl(0, "Comment", { ctermfg = 6 })
-		vim.api.nvim_set_hl(0, "Repeat", { ctermfg = 5 })
-		vim.api.nvim_set_hl(0, "StorageClass", { ctermfg = 5 })
-		-- vim.api.nvim_set_hl(0, "Macro", { ctermfg = 3 })
-		vim.api.nvim_set_hl(0, "Identifier", { ctermfg = 3 })
-		vim.api.nvim_set_hl(0, "Character", { ctermfg = 3 })
+		-- vim.api.nvim_set_hl(0, "Repeat", { ctermfg = 5 })
+		-- vim.api.nvim_set_hl(0, "StorageClass", { ctermfg = 5 })
+		-- -- vim.api.nvim_set_hl(0, "Macro", { ctermfg = 3 })
+		-- vim.api.nvim_set_hl(0, "Identifier", { ctermfg = 3 })
+		-- vim.api.nvim_set_hl(0, "Character", { ctermfg = 3 })
 		vim.api.nvim_set_hl(0, "SignColumn", { ctermfg = 0, ctermbg = 0 })
 		vim.api.nvim_set_hl(0, "LineNr", { ctermfg = 8, ctermbg = 0 })
-		-- vim.api.nvim_set_hl(0, 'SignColumn', { ctermfg = 0 })
+		-- vim.api.nvim_set_hl(0, "CursorLineNr", { ctermfg = 1, cterm = bold })
 		-- Make it clearly visible which argument we're at.
 		local marked = vim.api.nvim_get_hl(0, { name = "PMenu" })
-		vim.api.nvim_set_hl(0, "LspSignatureActiveParameter", {})
+		vim.api.nvim_set_hl(
+			0,
+			"LspSignatureActiveParameter",
+			{ fg = marked.fg, bg = marked.bg, ctermfg = marked.ctermfg, ctermbg = marked.ctermbg, bold = true }
+		)
 	end,
 }
